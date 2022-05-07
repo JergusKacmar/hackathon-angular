f8de0053ec2a205ecd05d08ee05d833c4d049f62
rastislav.vangor <rastislav.vangor@t-systems.com>
2022-05-07 15:31:50 +0200

    feat: :sparkles: working map data fetching

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,73 +1,93 @@
-// import { Component, OnInit } from '@angular/core';
-// import * as L from 'leaflet'
-
-// @Component({
-//   selector: 'app-map',
-//   templateUrl: './map.component.html',
-//   styleUrls: ['./map.component.scss']
-// })
-// export class MapComponent implements OnInit {
-//   constructor() { }
-
-//   ngOnInit(): void { }
-// }
-
-import { Component, AfterViewInit, Input } from '@angular/core';
-import {featureGroup, latLng, tileLayer, polygon, marker, Icon, LatLngBounds} from 'leaflet';
+import { Component, AfterViewInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { JobService } from 'src/app/shared/services/jobs.service';
+import { Job } from 'src/app/shared/services/job';
 import * as L from 'leaflet';
+import { map } from 'rxjs';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
+  providers: [JobService],
 })
 export class MapComponent implements AfterViewInit {
-  constructor() { }
+  constructor(public authService: AuthService, public jobService: JobService) {}
 
-  
+  jobs?: Job[];
+  private mapicka;
+  markerArray: Array<any>;
 
-  private map;
-
-  
- 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 48.71365, 21.25663 ],
-      zoom: 17
+    this.retrieveJobs();
+    this.mapicka = L.map('map', {
+      center: [48.71365, 21.25663],
+      zoom: 17,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19.9,
-      minZoom: 7,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19.9,
+        minZoom: 7,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
     const icon = {
       icon: L.icon({
-        iconSize: [ 25, 41 ],
-        iconAnchor: [ 13, 0 ],
+        iconSize: [25, 41],
+        iconAnchor: [13, 0],
         // specify the path here
         iconUrl: './assets/images/marker-icon.png',
-        shadowUrl: './assets/images/marker-shadow.png'
-     })
-  };
-  // const marker_current = L.marker([48.71365, 21.25663], icon).addTo(this.map)
-  // .bindPopup('Your Location<br> Kasárne/Kulturpark').openPopup();
-  // const marker_1 = L.marker([48.71365, 38.25663], icon).addTo(this.map)
-  // .bindPopup('Job 1<br> -address-').openPopup();
-  const marker1 = marker([48.71365, 21.25663], icon).bindPopup('Your Location<br> Kasárne/Kulturpark').openPopup();
-  const marker2 = marker([48.71365, 21.2], icon).bindPopup('Job 1<br> __address__').openPopup();
-  const group = featureGroup([marker1, marker2]);
-
-  group.addTo(this.map);
-  // marker_current.addTo(this.map);
-  // marker_1.addTo(this.map);
-  tiles.addTo(this.map);
-
+        shadowUrl: './assets/images/marker-shadow.png',
+      }),
+    };
   }
-
-  
 
   ngAfterViewInit(): void {
     this.initMap();
+  }
+
+  retrieveJobs(): void {
+    this.jobService
+      .getAll()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      )
+      .subscribe((data: any) => {
+        this.jobs = data;
+
+        const tiles = L.tileLayer(
+          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            maxZoom: 19.9,
+            minZoom: 7,
+            attribution:
+              '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          }
+        );
+
+        const icon = {
+          icon: L.icon({
+            iconSize: [25, 41],
+            iconAnchor: [13, 0],
+            iconUrl: './assets/images/marker-icon.png',
+            shadowUrl: './assets/images/marker-shadow.png',
+          }),
+        };
+
+        data.forEach((job) => {
+          const markerObj = L.marker([job.geoLat, job.geoLon], icon)
+            .bindPopup(job.title)
+            .addTo(this.mapicka);
+        });
+
+        tiles.addTo(this.mapicka);
+      });
   }
 }
